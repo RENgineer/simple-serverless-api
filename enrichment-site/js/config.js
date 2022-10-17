9ebe004d7c87d48e44d3f100572a4517f9e605ff
Lauren W <73594207+RENgineer@users.noreply.github.com>
2022-10-17 09:48:09 -0400

    Create config.js

---
+++ enrichment-site/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+  cognito: {
+    userPoolId: 'us-east-1_XeiPezgBP',
+    userPoolClientId: '7ag3c503kigrd383pepg5pkpon',
+    region: 'us-east-1'
+  },
+  api: {
+    invokeUrl: ''
+  }
+];
