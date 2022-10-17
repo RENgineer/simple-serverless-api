d9444ef48b6c0db55e57d614e95afc210f82150f
Lauren W <73594207+RENgineer@users.noreply.github.com>
2022-10-17 15:05:58 -0400

    Update config.js

--- enrichment-site/js/config.js
+++ enrichment-site/js/config.js
@@ -5,6 +5,6 @@ window._config = {
     region: 'us-east-1'
   },
   api: {
-    invokeUrl: 'https://8qom5xfp4b.execute-api.us-east-1.amazonaws.com/test'
+    invokeUrl: 'https://8qom5xfp4b.execute-api.us-east-1.amazonaws.com/prod'
   }
 ];
